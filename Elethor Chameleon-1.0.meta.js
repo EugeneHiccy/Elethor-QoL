cc18a600d8520b4810c3385ae4cba68419644844
EugeneHiccy <hiccyeugene@outlook.com>
2024-11-02 13:19:43 +0530

    Create Elethor Chameleon-1.0.meta.js

---
+++ Elethor Chameleon-1.0.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Elethor Chameleon
+// @namespace    http://tampermonkey.net/
+// @version      1.0 // Update this version number with each release
+// @description  Change colors on Elethor.com
+// @author       Eugene
+// @match        *://elethor.com/*
+// @grant        GM_addStyle
+// @downloadURL  https://raw.githubusercontent.com/EugeneHiccy/Elethor-QoL/main/Elethor%20Chameleon-1.0.user.js
+// @updateURL    https://raw.githubusercontent.com/EugeneHiccy/Elethor-QoL/main/Elethor%20Chameleon-1.0.meta.js
+// ==/UserScript==
