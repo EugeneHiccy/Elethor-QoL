1e8da5caeb4bf7d753fd5fce5cb16898100cac1d
EugeneHiccy <hiccyeugene@outlook.com>
2024-11-02 12:41:12 +0530

    Update Elethor Color Changer-1.0.user.js

--- Elethor Color Changer-1.0.user.js
+++ Elethor Color Changer-1.0.user.js
@@ -2,11 +2,28 @@
 // @name         Elethor Color Changer
 // @namespace    http://tampermonkey.net/
 // @version      1.0
+// @author       Eugene
 // @description  Change colors on Elethor.com
 // @match        *://elethor.com/*
 // @grant        GM_addStyle
+// @license      GPL-3.0-or-later
+// @downloadURL  https://raw.github.com/EugeneHiccy/Elethor-QoL/main/Elethor%20Color%20Changer-1.0.user.js
+// @updateURL    https://raw.github.com/EugeneHiccy/Elethor-QoL/main/Elethor%20Color%20Changer-1.0.user.js
 // ==/UserScript==
-
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 (function() {
     'use strict';
 
